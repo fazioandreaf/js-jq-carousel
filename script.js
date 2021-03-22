9c174119df94d0fedda596f96a876058673bda79
fazioandrea.f <fazioandrea.f@gmail.com>
2021-03-22 16:21:13 +0100

    Esercizio finito

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 function init(){
     $('.next').click(function (){
-        var immagineattuale=$('.active')
-        var immagineprossima=$('.active').next()
+        var immagineattuale=$('.active');
+        var immagineultima=$('.images img:last-child');
+        var immagineprossima=$('.active').next();
+        console.log(immagineultima,immagineattuale)
+        if(immagineultima.hasClass('active')){
+            immagineprossima=$('.images img:first-child')
+        }
+        immagineattuale.removeClass('active')
+        immagineprossima.addClass('active')
+    })
+    $('.prev').click(function (){
+        var immagineattuale=$('.active');
+        var immagineprima=$('.images img:first-child');
+        var immagineprossima=$('.active').prev();
+        console.log(immagineprima,immagineattuale)
+        if(immagineprima.hasClass('active')){
+            immagineprossima=$('.images img:last-child')
+        }
         immagineattuale.removeClass('active')
         immagineprossima.addClass('active')
     })
