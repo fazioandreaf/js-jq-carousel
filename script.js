42aa6ea6627daad5ef5f36ed2f3d7e783311349b
fazioandrea.f <fazioandrea.f@gmail.com>
2021-03-22 18:03:59 +0100

    Prova per la creazione dei pallini in modo automatico

--- script.js
+++ script.js
@@ -3,47 +3,42 @@ function init(){
         var immagineattuale=$('.images .active');
         var immagineultima=$('.images img:last-child');
         var immagineprossima=$('.images .active').next();
+        var pallinoattuale=$('.pallini .active');
+        var pallinoprossima=$('.pallini .active').next();
         if(immagineultima.hasClass('active')){
             immagineprossima=$('.images img:first-child')
+            pallinoprossima=$('.pallini i:first-child')
         }
         immagineattuale.removeClass( 'active')
         immagineprossima.addClass( 'active')
+        pallinoattuale.removeClass('active')
+        pallinoprossima.addClass('active')
     })
     $('.prev').click(function (){
         var immagineattuale=$('.images .active');
         var immagineprima=$('.images img:first-child');
         var immagineprossima=$('.images .active').prev();
+        var pallinoattuale=$('.pallini .active');
+        var pallinoprossima=$('.pallini .active').prev();
         if(immagineprima.hasClass('active')){
             immagineprossima=$('.images img:last-child')
+            pallinoprossima=$('.pallini i:last-child')
         }
         immagineattuale.removeClass('active')
         immagineprossima.addClass('active')
-    })
-
-    $('.next').click(function (){
-        var pallinoattuale=$('.pallini .active');
-        var pallinoultima=$('.pallini i:last-child ');
-        var pallinoprossima=$('.pallini .active').next();
-        console.log(pallinoultima,pallinoattuale,pallinoprossima)
-        if(pallinoultima.hasClass('active')){
-            pallinoprossima=$('.pallini i:first-child')
-            console.log('prova')
-        }
         pallinoattuale.removeClass('active')
         pallinoprossima.addClass('active')
     })
-    $('.prev').click(function (){
-        var pallinoattuale=$('.pallini .active');
-        var pallinoultima=$('.pallini i:first-child ');
-        var pallinoprossima=$('.pallini .active').prev();
-        console.log(pallinoultima,pallinoattuale,pallinoprossima)
-        if(pallinoultima.hasClass('active')){
-            pallinoprossima=$('.pallini i:last-child')
-            console.log('prova')
+    function creazione_li() {
+        let numberIMG=$('.images>img').length;
+        for(i=0;i<numberIMG;i++){
+            $('.pallini').html('<i class="fa fa-circle active" aria-hidden="true" data-value= '+ (i+1)+' ></i>')
+            console.log(prova)
+        
         }
-        pallinoattuale.removeClass('active')
-        pallinoprossima.addClass('active')
-    })
 
+        console.log('ciao')
+    }
+    creazione_li()
 }
 $(document).ready(init)
